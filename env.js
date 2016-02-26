5cad170ab9fd65f2658044f6ffef5fa42d134fe3
Rodrigue Villetard <rodrigue.villetard@sacristi.fr>
2016-02-26 23:04:24 +0100

    clean

--- env.js
+++
@@ -1,5 +0,0 @@
-(function (window) {
-                window.__env = window.__env || {};
-                 window.__env.AUTH0_CLIENT_ID = 'AIE34534534534534';
- window.__env.API_URL = 'http://dockerhost:3000/app/api.php?UIE';
- }(this));
